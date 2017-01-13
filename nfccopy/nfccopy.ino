f4a723a849d98c4f781c9a660c33be61fa5a2697
thewass <lord.d35ruct0@gmail.com>
2017-01-13 18:05:13 -0500

    Completed write command
    Still has issues writing first 4 pages.

--- nfccopy/nfccopy.ino
+++ nfccopy/nfccopy.ino
@@ -13,9 +13,9 @@ char rorw;
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.println("set baud=115200");
-  Serial.end();
-  Serial.begin(115200);
+  //Serial.println("set baud=115200");
+  //Serial.end();
+  //Serial.begin(115200);
 
   nfc.begin();
 
@@ -41,57 +41,75 @@ void loop(void) {
   
   Serial.println("r or w?");
   Serial.flush();
-  if (Serial.available()) {
+  while (!Serial.available());
+  while (Serial.available()) {
     rorw = Serial.read();
-    if (rorw == 'w')
-    {
-      
-    } 
-    else if ( rorw == 'r')
-    {
-     
-    }
   }
-
-  if (false)
-  {
-    // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
-    // the UID, and uidLength will indicate the size of the UUID (normally 7)
-    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
+  // the UID, and uidLength will indicate the size of the UUID (normally 7)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  if (success) {
+    Serial.print("UID: ");
+    nfc.PrintHex(uid, uidLength);
     
-    if (success) {
-      Serial.print("UID: ");
-      nfc.PrintHex(uid, uidLength);
-      Serial.println("");
+    if (uidLength == 7)
+    {
+      // NTAG2xx cards have 39*4 bytes of user pages (156 user bytes),
+      // starting at page 4 ... larger cards just add pages to the end of
+      // this range:
       
-      if (uidLength == 7)
+      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
+  
+      // TAG Type       PAGES   USER START    USER STOP
+      // --------       -----   ----------    ---------
+      // NTAG 203       42      4             39
+      // NTAG 213       45      4             39
+      // NTAG 215       135     4             129
+      // NTAG 216       231     4             225      
+
+      if (rorw == 'w')
       {
-        // NTAG2xx cards have 39*4 bytes of user pages (156 user bytes),
-        // starting at page 4 ... larger cards just add pages to the end of
-        // this range:
+        // TODO:  Make it work with the path.
+        myfile = SD.open(filename, FILE_READ);
+        myfile.seek(0);
+        Serial.print("Reading from ");
+        Serial.println(filename);
+        for (uint8_t i = 0; i < 135; i++) 
+        {
+          //Read page from file
+          myfile.read(dataBuffer, 4);
+          //Write page to tag
+          //First 4 pages are write protected.  Need special function.
+          
+          success = nfc.ntag2xx_WritePage(i, dataBuffer);
+          if (success) {
+            //nfc.PrintHexChar(dataBuffer, 4);
+          }
+          else 
+          {
+            Serial.println("Write Fail");
+          }
+        }//EndFor
         
-        // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
-  
-        // TAG Type       PAGES   USER START    USER STOP
-        // --------       -----   ----------    ---------
-        // NTAG 203       42      4             39
-        // NTAG 213       45      4             39
-        // NTAG 215       135     4             129
-        // NTAG 216       231     4             225      
-  
+        myfile.close();
+      } //EndWrite
+      else if (rorw == 'r')
+      {
+        // TODO:  Make it work with the path.
         SD.remove(filename);
         myfile = SD.open(filename, FILE_WRITE);
-        Serial.print("Writing ");
+        myfile.seek(0);
+        Serial.print("Saving to ");
         Serial.println(filename);
+        // 
         nfc.ntag2xx_ReadPage(0, dataBuffer); // Sometimes, the first read is too fast :)
         for (uint8_t i = 0; i < 135; i++) 
         {
           success = nfc.ntag2xx_ReadPage(i, dataBuffer);
-          
           if (success) 
           {
             // Dump the page data
-            nfc.PrintHexChar(dataBuffer, 4);
+            //nfc.PrintHexChar(dataBuffer, 4);
             myfile.write(dataBuffer, 4);
           }
           else
@@ -99,29 +117,26 @@ void loop(void) {
             Serial.println("Read Fail");
           }
         }//EndFor
-        myfile.flush();
-        myfile.seek(0);
-        Serial.println("");
-        while(myfile.available()){
-          myfile.read(dataBuffer, 4);
-          nfc.PrintHexChar(dataBuffer, 4);
-        }
+    
+        //Print file contents for confirmation
+        //myfile.flush();
+        //myfile.seek(0);
+        //Serial.println("");
+        //while(myfile.available()){
+        //  myfile.read(dataBuffer, 4);
+        //  nfc.PrintHexChar(dataBuffer, 4);
+        //}
+        //EndPrintFile
+        
         myfile.close();
-      }
-      else
-      {
-        Serial.println("Tag Fail");
-      }
-      
-      // Wait a bit before trying again
-      Serial.println("\nSuccess!\nSend char for new.");
-      Serial.flush();
-      while (!Serial.available());
-      while (Serial.available()) {
-        Serial.read();
-      }
-      Serial.flush();
+      } //EndRead
     }
+    // Wait a bit before trying again
+    Serial.println("Complete!\n");
+  }
+  else
+  {
+    Serial.println("Tag Fail");
   }
 }
 
